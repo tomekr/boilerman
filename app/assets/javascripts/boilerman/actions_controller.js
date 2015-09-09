d77e650c27b1ba6cf507748a7912aadb5a27459b
Tomek Rabczak <tomek.rabczak@gmail.com>
2015-09-09 17:27:12 +0100

    before dinner

--- app/assets/javascripts/boilerman/actions_controller.js
+++ app/assets/javascripts/boilerman/actions_controller.js
@@ -20,6 +20,46 @@ function send_with_filter() {
   }
 }
 
+
+function send_controller_filter() {
+  var filter_input = $("#with_filter_input")[0];
+
+  // we should only apply new query string if there is actual input
+  if (filter_input.value.length) {
+    // decide which seperator we want depending on if there is a current query string
+    var seperator = window.location.search ? "&" : "?";
+
+    // Get current URL
+    var href = window.location.href;
+    // append new query
+    new_link = href + seperator + "filters[with_filters][]=" + filter_input.value;
+
+    window.location = new_link;
+  } else {
+    alert("You did not specify a filter to apply");
+  }
+}
+
+
+function send_without_filter() {
+  var filter_input = $("#without_filter_input")[0];
+
+  // we should only apply new query string if there is actual input
+  if (filter_input.value.length) {
+    // decide which seperator we want depending on if there is a current query string
+    var seperator = window.location.search ? "&" : "?";
+
+    // Get current URL
+    var href = window.location.href;
+    // append new query
+    new_link = href + seperator + "filters[without_filters][]=" + filter_input.value;
+
+    window.location = new_link;
+  } else {
+    alert("You did not specify a filter to apply");
+  }
+}
+
 function remove_with_filter(list_item) {
   var filter_text = list_item.text.trim();
   var original_query = window.location.search;
@@ -43,5 +83,25 @@ function remove_with_filter(list_item) {
 }
 
 function remove_without_filter(list_item) {
-  // TODO
+  console.log("remove_without_filter");
+  var filter_text = list_item.text.trim();
+  var original_query = window.location.search;
+
+  console.log(filter_text);
+  var question_regex  = new RegExp("\\?filters\\[without_filters\\]\\[\\]=" + filter_text + "&*");
+  var ampersand_regex = new RegExp("&filters\\[without_filters\\]\\[\\]=" + filter_text);
+
+  var query = "";
+  if (question_regex.test(original_query)) {
+    console.log("we've got a question mark");
+    query = original_query.replace(question_regex, '?');
+
+  } else if (ampersand_regex.test(original_query)) {
+    console.log("we've got an ampersand mark");
+    query = original_query.replace(ampersand_regex, '');
+  }
+
+  console.log(query);
+  // reload page with new query string
+  window.location = window.location.href.split('?')[0] + query;
 }
