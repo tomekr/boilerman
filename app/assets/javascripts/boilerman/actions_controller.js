4e6323e5241eb05f89cec04dc5f71e82a96d4399
Tomek Rabczak <tomek.rabczak@gmail.com>
2015-09-10 11:42:55 +0100

    commit before food came out

--- app/assets/javascripts/boilerman/actions_controller.js
+++ app/assets/javascripts/boilerman/actions_controller.js
@@ -1,6 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$( document ).ready(function() {
+  // Initialize global variables
+  window.controller_filters = [];
+
+  // When the enter key is pressed on the filter input boxes, trigger the
+  // onclick event for that text box
+  $("#controller_filter_input").keyup(function(event){
+      if(event.keyCode == 13){
+          $("#controller_filter_input_btn").click();
+      }
+  });
+
+  $("#with_filter_input").keyup(function(event){
+      if(event.keyCode == 13){
+          $("#action_with_filter_input_btn").click();
+      }
+  });
+
+  $("#without_filter_input").keyup(function(event){
+      if(event.keyCode == 13){
+          $("#action_without_filter_input_btn").click();
+      }
+  });
+});
+
 function send_with_filter() {
   var filter_input = $("#with_filter_input")[0];
 
@@ -22,7 +47,7 @@ function send_with_filter() {
 
 
 function send_controller_filter() {
-  var filter_input = $("#with_filter_input")[0];
+  var filter_input = $("#controller_filter_input")[0];
 
   // we should only apply new query string if there is actual input
   if (filter_input.value.length) {
@@ -32,7 +57,7 @@ function send_controller_filter() {
     // Get current URL
     var href = window.location.href;
     // append new query
-    new_link = href + seperator + "filters[with_filters][]=" + filter_input.value;
+    new_link = href + seperator + "filters[controller_filters][]=" + filter_input.value;
 
     window.location = new_link;
   } else {
